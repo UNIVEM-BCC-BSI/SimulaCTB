339cd5615dc419949d8a2e05516307cd885984ab
Matheus05CAlves <162243482+Matheus05CAlves@users.noreply.github.com>
2024-05-03 21:26:32 -0300

    Add files via upload

--- BDV1.py
+++ BDV1.py
@@ -1,13 +1,34 @@
 import sqlite3
 
-banco = sqlite3.connect('primeiro_banco')
+conn = sqlite3.connect('questionario.db')
 
-cursor = banco.cursor()
+cursor = conn.cursor()
 
-#cursor.execute("CREATE TABLE questões (N°QUESTÃO text, RESPOSTA text)")
-#cursor.execute("INSERT INTO questões VALUES('vapoktl','Resposta A')")
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Perguntas (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    pergunta TEXT,
+    opcao_a TEXT,
+    opcao_b TEXT,
+    opcao_c TEXT,
+    opcao_d TEXT,
+    resposta_correta TEXT
+)
+''')
+perguntas = [
+    ("A atual legislação de trânsito intitula-se:", "A) Código Nacional de Trânsito", "B) Código de Trânsito", "C) Código de Trânsito Brasileiro", "D) Código Brasileiro de Trânsito", "C"),
+    ("02) O trânsito de qualquer natureza nas vias terrestres do território nacional, abertas à circulação, rege-se pelo:'?", "A) William Shakespeare", "B) Miguel de Cervantes", "C) Jorge Luis Borges",
+     "D) Machado de Assis", "B"),
+    
+]
 
-#banco.commit()
+for pergunta in perguntas:
+    cursor.execute(
+        'INSERT INTO Perguntas (pergunta, opcao_a, opcao_b, opcao_c, opcao_d, resposta_correta) VALUES (?, ?, ?, ?, ?, ?)',
+        pergunta)
+conn.commit()
 
-cursor.execute("SELECT * FROM questões")
+print("Banco de dados criado e perguntas inseridas com sucesso!")
+
+cursor.execute("SELECT * FROM Perguntas")
 print(cursor.fetchall())
